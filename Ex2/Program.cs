e1a91594c12a58a04d5110d503bd0f6fbc465c22
Nessa <la-nessa@mail.ru>
2022-12-06 00:04:34 +0300

    Ex2

---
+++ Ex2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    string number2 = Convert.ToString(number);
+    Console.WriteLine(number2[2]);
+}
