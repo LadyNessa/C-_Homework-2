48c4c11fbb32cd1bfc16c5ab02b5907c406a5834
Nessa <la-nessa@mail.ru>
2022-12-06 00:17:38 +0300

    Ex3

---
+++ Ex3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 7 == 0)
+{
+    if (number % 23 == 0)
+    {
+        Console.WriteLine("Число кратно одновременно 7 и 23");
+    }
+}
+else
+{
+    Console.WriteLine("Число не кратно одновременно 7 и 23");
+}
