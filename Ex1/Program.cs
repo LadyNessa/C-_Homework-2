6be01eccb435abeaaab9b882e4e457c9a21d02d7
Nessa <la-nessa@mail.ru>
2022-12-05 23:22:51 +0300

    Ex1

---
+++ Ex1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int secondDigit = (number / 10) % 10;
+Console.Write("Вторая цифра: ");
+Console.WriteLine(secondDigit);
